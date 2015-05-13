4e90cd6916aff8ba9f6ab5b30c79581908e67307
laurent <lbe.georges@gmail.com>
2015-05-13 23:13:30 +0200

    modif menu deplacement

--- src/com/arduino/robalim/view/MenuDeplacement.java
+++ src/com/arduino/robalim/view/MenuDeplacement.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Switch;
 import at.abraxas.amarino.AmarinoIntent;
 
@@ -27,56 +28,63 @@ public class MenuDeplacement extends Activity {
         
         setContentView(R.layout.menu_deplacement);
         activity=this;
-        Button button_go = (Button)findViewById(R.id.button_go);
-        Button button_left = (Button)findViewById(R.id.button_left);
-        Button button_right = (Button)findViewById(R.id.button_right);
-        Button button_stop = (Button)findViewById(R.id.button_stop);
-        final Switch vitesse_rapide = (Switch)findViewById(R.id.switch_motion);
+        ImageButton button_up_left = (ImageButton)findViewById(R.id.button_up_left);
+        ImageButton button_up_fast = (ImageButton)findViewById(R.id.button_up_fast);
+        ImageButton button_up_right = (ImageButton)findViewById(R.id.button_up_right);
+        ImageButton button_left = (ImageButton)findViewById(R.id.button_left);
+        ImageButton button_up = (ImageButton)findViewById(R.id.button_up);
+        ImageButton button_right = (ImageButton)findViewById(R.id.button_right);
+        ImageButton button_stop = (ImageButton)findViewById(R.id.button_stop);
         
-        button_go.setOnClickListener(new OnClickListener() {
-        	
+        button_up_left.setOnClickListener(new OnClickListener() {
         	@Override
         	public void onClick(View v) {
-        		if(vitesse_rapide.isChecked())
-        			robot_out.avancerRapidement(activity);
-        		else
-        			robot_out.avancer(activity);
+        		robot_out.tournerAGaucheRapidement(activity);
         	}
         });
         
+        button_up_right.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		robot_out.tournerADroiteRapidement(activity);
+        	}
+        });
+        
+        button_up_fast.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		robot_out.avancerRapidement(activity);
+        	}
+        });
         
         button_left.setOnClickListener(new OnClickListener() {
-        	
         	@Override
         	public void onClick(View v) {
-        		if(vitesse_rapide.isChecked())
-        			robot_out.tournerAGaucheRapidement(activity);
-        		else
-        			robot_out.tournerAGauche(activity);
+        		robot_out.tournerAGauche(activity);
         	}
         });
         
+        button_up.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		robot_out.avancer(activity);
+        	}
+        });
         
         button_right.setOnClickListener(new OnClickListener() {
-        	
         	@Override
         	public void onClick(View v) {
-        		if(vitesse_rapide.isChecked())
-        			robot_out.tournerADroiteRapidement(activity);
-        		else
-        			robot_out.tournerADroite(activity);
+        		robot_out.tournerADroite(activity);
         	}
         });
         
-        
         button_stop.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-					robot_out.stopper(activity);
-			}
-		});
-       
+        	@Override
+        	public void onClick(View v) {
+        			robot_out.stopper(activity);
+        	}
+        });
+        
 	}
 
 	@Override
